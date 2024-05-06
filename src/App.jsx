c344f40f2debe58b835b19cfe427eb12d150b16f
Shamoulox <charly.tonnel@gmail.com>
2024-05-06 17:16:38 +0200

    scss ok app.jsx ok parametrage dossier ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import data from "./logements.json";
-
+import './styles/pages/styles.css'
 import Home from "./pages/Home";
 import About from "./pages/About";
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Footer from "../components/Footer";
 
+
+
+
 const Home = () => {
   return (
     <div>
