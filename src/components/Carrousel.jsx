2daca0d05f5999d14386c23d31234a371b2e3269
Shamoulox <charly.tonnel@gmail.com>
2024-05-24 18:36:30 +0200

    ajout carroussel ok

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+const Carousel = ({ pics }) => {
+  const [index, setIndex] = useState(0);
+
+  const nextPics = () => {
+    setIndex(index + 1);
+    if (index === pics.length - 1) {
+      setIndex(0);
+    }
+  };
+
+  const prevPics = () => {
+    setIndex(index - 1);
+    if (index === 0) {
+      setIndex(pics.length - 1);
+    }
+  };
+
+  return (
+    <div className="carrousel">
+      <img src={pics[index]} alt={pics[index]} />
+      {pics.length > 1 && (
+        <div className="chevrons">
+          <i className="fa-solid fa-chevron-left" onClick={prevPics}></i>
+          <i className="fa-solid fa-chevron-right" onClick={nextPics}></i>
+          <p>
+            {index + 1} / {pics.length}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,26 +1,23 @@
 import { useParams } from "react-router-dom";
-import Banner from "../components/Banner";
+
 import Boards from "../components/Boards";
 import datajson from "../logements.json";
 import React from "react";
-
-
-
+import Carousel from "../components/Carrousel";
 
 function Housing() {
-    const { id } = useParams();
-    const logementDatas = datajson.find((logement) => logement.id === id)
+  const { id } = useParams();
+  const logementDatas = datajson.find((logement) => logement.id === id);
   return (
-
     <div>
-      <Banner></Banner>
+      <Carousel pics={logementDatas.pictures}/>
+
       <Boards
         title={logementDatas.title}
         location={logementDatas.location}
         host={logementDatas.host}
         rating={logementDatas.rating}
       />
-      
     </div>
   );
 }
